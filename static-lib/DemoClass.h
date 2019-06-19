40987f14cf874f128bb3c7638e2a38c23fbdfd8d
czoido <mrgalleta@gmail.com>
2019-06-19 14:22:56 +0200

    export symbols

--- static-lib/DemoClass.h
+++ static-lib/DemoClass.h
@@ -2,7 +2,16 @@
 
 #include <string>
 
-class DemoClass
+
+#if defined(_MSC_VER)
+	#define SO_IMPORT __declspec(dllimport)
+	#define SO_EXPORT __declspec(dllexport)
+#else
+	#define SO_IMPORT
+	#define SO_EXPORT
+#endif
+
+class SO_EXPORT DemoClass
 {
 public:
 	DemoClass();
